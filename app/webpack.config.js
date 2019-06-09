393e77a7aae8da70141bfb95ae3362f9cdc3d40c
Ilias Apostolou <ilias.apostolou.zero@gmail.com>
2019-06-09 15:54:03 +0300

    Fixed webpack-dev-server inconsistencies + included bootstrap.min.css.

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,6 +1,14 @@
+
+const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+    //rules: [
+    //    
+    //],
+    //devServer: {
+    //    contentBase: path.join(__dirname, './dist'),
+    //},
     plugins: [
         new CopyPlugin([
             {from: 'assets', to: '.'}

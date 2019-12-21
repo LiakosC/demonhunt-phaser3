92599a3f3055c205b49453bde4f7198ff561168a
Ilias Apostolou <ilias.apostolou.zero@gmail.com>
2019-12-21 19:24:42 +0200

    `menu.` to `this.`.

--- app/src/scenes/MenuScene.js
+++ app/src/scenes/MenuScene.js
@@ -9,38 +9,38 @@ export class MenuScene extends BasicScene {
 
     create() {
         //boot.createKeys();
-		ph.add.sprite(0, 0, "menu-image");
-		menu.buttonHoverSound = ph.add.audio("button-hover");
+		this.add.sprite(0, 0, "menu-image");
+		this.buttonHoverSound = this.add.audio("button-hover");
 		if (MENU_MUSIC) {
-			menu.music = ph.add.audio("menu-music", 1, true);
-			menu.music.play();
+			this.music = this.add.audio("menu-music", 1, true);
+			this.music.play();
 		}
 		
-		menu.box = $("<div>").addClass("menuBox").appendTo(htmlbox)[0];
-		//menu.box.innerHTML = '<div id="innerDiv"></div>';
+		this.box = $("<div>").addClass("menuBox").appendTo(htmlbox)[0];
+		//this.box.innerHTML = '<div id="innerDiv"></div>';
 		
-		menu.BK = new Book();
-		menu.BK.pageStart("main", function() {
+		this.BK = new Book();
+		this.BK.pageStart("main", function() {
 			console.log("book main");
-			$(menu.box).addClass("main").html('\
+			$(this.box).addClass("main").html('\
 				<button>Profiles</button>\
 				<button>Options</button>\
 				<button>Cinematic</button>\
 				<button>Exit</button>\
 			');
-			$(menu.box).find("button").eq(0).on("click", function() {menu.BK.start("profiles");});
-			$(menu.box).find("button").eq(1).on("click", function() {menu.BK.start("options");});
-			$(menu.box).find("button").eq(2).on("click", function() {cinematic.start();});
-			$(menu.box).find("button").eq(3).on("click", function() {exit();});
-			$(menu.box).find("button").on("mouseover", function() {menu.buttonHoverSound.play();});
-			menu.BK.pageEnd("main", function() {
-				$(menu.box).removeClass("main").html('');
+			$(this.box).find("button").eq(0).on("click", function() {this.BK.start("profiles");});
+			$(this.box).find("button").eq(1).on("click", function() {this.BK.start("options");});
+			$(this.box).find("button").eq(2).on("click", function() {cinematic.start();});
+			$(this.box).find("button").eq(3).on("click", function() {exit();});
+			$(this.box).find("button").on("mouseover", function() {this.buttonHoverSound.play();});
+			this.BK.pageEnd("main", function() {
+				$(this.box).removeClass("main").html('');
 			});
 		});
 		
 		// PROFILES
-		menu.BK.pageStart("profiles", function() {
-			$(menu.box).addClass("profiles").html('\
+		this.BK.pageStart("profiles", function() {
+			$(this.box).addClass("profiles").html('\
 			<div class="list">\
 				<div></div>\
 			</div>\
@@ -55,7 +55,7 @@ export class MenuScene extends BasicScene {
 				<button>Back</button>\
 			</div>\
 			');
-			var list = $(menu.box).find(".list > div")[0];
+			var list = $(this.box).find(".list > div")[0];
 			var insertProfile = function(name) {
 				var prof = $("<div>").addClass("profile").html(name).on("mousedown", function() {
 					var profiles = list.querySelectorAll(".profile");
@@ -71,76 +71,76 @@ export class MenuScene extends BasicScene {
 			}
 			
 			// Load button
-			$(menu.box).find(".buttons button").eq(0).on("click", function() {
+			$(this.box).find(".buttons button").eq(0).on("click", function() {
 				var selected = list.querySelector(".selected");
 				if (selected != null) {
 					var profileName = selected.innerHTML;
 					boot.SetProfile(profileName);
 					//memory.profile = profileName;
 					console.log("Profile selected: ", boot.profileMemoryData);
-					menu.BK.start("episodes");
+					this.BK.start("episodes");
 				}
 			});
-			$(menu.box).find(".back button").on("click", function() {menu.BK.start("main");});
-			menu.BK.pageEnd("profiles", function() {
-				$(menu.box).removeClass("profiles").html('');
+			$(this.box).find(".back button").on("click", function() {this.BK.start("main");});
+			this.BK.pageEnd("profiles", function() {
+				$(this.box).removeClass("profiles").html('');
 			});
 		});
 		
-		menu.BK.pageStart("options", function() {
-			$(menu.box).addClass("options").html('\
+		this.BK.pageStart("options", function() {
+			$(this.box).addClass("options").html('\
 				<div>Movement: A D</br>Attack: Left/Right arrows</br>Jump: W</br>Action: Spacebar</br>Speak: Enter</div>\
 				<div class="back"><button>Back</button></div>\
 			');
-			$(menu.box).find("button").eq(0).on("click", function() {menu.BK.start("main");});
-			$(menu.box).find("button").on("mouseover", function() {menu.buttonHoverSound.play();});
-			menu.BK.pageEnd("options", function() {
-				$(menu.box).removeClass("options").html('');
+			$(this.box).find("button").eq(0).on("click", function() {this.BK.start("main");});
+			$(this.box).find("button").on("mouseover", function() {this.buttonHoverSound.play();});
+			this.BK.pageEnd("options", function() {
+				$(this.box).removeClass("options").html('');
 			});
 		});
 		
 
 		// EPISODES
 		var choose_episode = -1;
-		menu.BK.pageStart("episodes", function() {
+		this.BK.pageStart("episodes", function() {
 			// before it starts, the profile data must be saved in boot.profileMemoryData (string). boot.SetProfile(profileName)
-			$(menu.box).addClass("episodes").html('\
+			$(this.box).addClass("episodes").html('\
 				<div><button>Trapped in Darkness</button></div>\
 				<div><button disabled="true">Blade of Souls</button></div>\
 				<div><button disabled="true">Archangel Betrayal</button></div>\
 				<div><button disabled="true">Apocalypse of the One</button></div>\
 				<div class="back"><button>Back</button></div>\
 			');
-			$(menu.box).find("button").eq(0).on("click", function() { // ep 1
-				menu.StartPage_levels(1);
+			$(this.box).find("button").eq(0).on("click", function() { // ep 1
+				this.StartPage_levels(1);
 			});
-			$(menu.box).find("button").eq(-1).on("click", function() { // back
-				menu.BK.start("profiles");
+			$(this.box).find("button").eq(-1).on("click", function() { // back
+				this.BK.start("profiles");
 			});
-			menu.BK.pageEnd("episodes", function() {
-				$(menu.box).removeClass("episodes").html('');
+			this.BK.pageEnd("episodes", function() {
+				$(this.box).removeClass("episodes").html('');
 			});
 		});
 		
-		menu.BK.pageStart("levels", function() {
+		this.BK.pageStart("levels", function() {
 			// befure it starts, var choose_episode is edited (first is 1)
 			// knows about choose_episode
-			$(menu.box).addClass("levels").html('\
+			$(this.box).addClass("levels").html('\
 				<div class="choosealevel">\
 					<div class="artifacts"></div>\
 					<div class="levels"></div>\
 				</div>\
 				<div class="back"><button>Back</button></div>\
 			');
-			$(menu.box).find("button").eq(-1).on("click", function() {menu.BK.start("episodes");});
+			$(this.box).find("button").eq(-1).on("click", function() {this.BK.start("episodes");});
 			
-			var choose_episode = menu.chosenEpisode;
+			var choose_episode = this.chosenEpisode;
 			var profileData = boot.profileMemoryData;
 			var episodeData = profileData.episodes[choose_episode]; // episode
 			
 			/* artifacts div */
 			
-			var ar_box = $(menu.box).find(".choosealevel .artifacts")[0];
+			var ar_box = $(this.box).find(".choosealevel .artifacts")[0];
 			var ar_count = 0; for (var artifactKey in episodeData["artifacts"]) {ar_count++;}
 			//console.log("artifacts count", ar_count);
 			var dx = ar_box.offsetWidth / (ar_count + 1);
@@ -174,11 +174,11 @@ export class MenuScene extends BasicScene {
 				img.src = GRAPHICS_ROOT+'/artifacts/'+file+'.png';
 				var artifactMouse = function(img, name) {
 					img.addEventListener("mouseover", function() {
-						var info = $(menu.box).find(".choosealevel .artifact_info").show()[0];
+						var info = $(this.box).find(".choosealevel .artifact_info").show()[0];
 						info.innerHTML = name; // artifact name
 					});
 					img.addEventListener("mouseout", function() {
-						var info = $(menu.box).find(".choosealevel .artifact_info").hide()[0];
+						var info = $(this.box).find(".choosealevel .artifact_info").hide()[0];
 					});
 				}
 				if (episodeData.artifacts[artifactName] == true) {
@@ -191,7 +191,7 @@ export class MenuScene extends BasicScene {
 				artifactsScan_i++;
 			}
 			//html_artifacts += '</tr></table>';
-			//$(menu.box).find(".choosealevel .artifacts")[0].innerHTML = html_artifacts;
+			//$(this.box).find(".choosealevel .artifacts")[0].innerHTML = html_artifacts;
 			
 			/* levels buttons div */
 			
@@ -246,11 +246,11 @@ export class MenuScene extends BasicScene {
 				}
 				
 				btn.addEventListener("click", function() {
-					menu.runGame(choose_episode, levelKey);
+					this.runGame(choose_episode, levelKey);
 				});
 				btn.addEventListener("mouseover", function() {
 					if (currentLevelData.completed) {
-						var info = $(menu.box).find(".choosealevel .info").show()[0];
+						var info = $(this.box).find(".choosealevel .info").show()[0];
 						var goldElement = info.querySelector(".text");
 						//var goldMax = boot.episodes[choose_episode]["levels"][levelKey].getTotalGold();
 						goldMax = boot.profileMemoryData.episodes[choose_episode].levels[levelKey].goldMax;
@@ -258,14 +258,14 @@ export class MenuScene extends BasicScene {
 					}
 				});
 				btn.addEventListener("mouseout", function() {
-					$(menu.box).find(".choosealevel .info").hide();
+					$(this.box).find(".choosealevel .info").hide();
 				});
 				
 				parent.appendChild(btn);
 				return btn;
 			}
 			
-			var box = $(menu.box).find(".choosealevel .levels")[0];
+			var box = $(this.box).find(".choosealevel .levels")[0];
 			for (var levelKey in episodeData.levels) {
 				var btn = createLevelButton(box, levelKey);
 			}
@@ -282,12 +282,12 @@ export class MenuScene extends BasicScene {
 			
 			
 			
-			menu.BK.pageEnd("levels", function() {
-				$(menu.box).removeClass("levels").html('');
+			this.BK.pageEnd("levels", function() {
+				$(this.box).removeClass("levels").html('');
 			});
 		});
 		
-		menu.callback();
+		this.callback();
     }
 
     update() {
@@ -295,18 +295,18 @@ export class MenuScene extends BasicScene {
     }
     
 	shutdown() {
-		if (menu.music != null) {
-			menu.music.destroy();
-			menu.music = null;
+		if (this.music != null) {
+			this.music.destroy();
+			this.music = null;
 		}
-		menu.BK.end();
-		menu.box.parentNode.removeChild(menu.box);
+		this.BK.end();
+		this.box.parentNode.removeChild(this.box);
 	}
 
     StartPage_levels(episodeKey) {
         if (episodeKey == undefined) throw "episodeKey " + episodeKey;
-        menu.chosenEpisode = 1;
-        menu.BK.start("levels");
+        this.chosenEpisode = 1;
+        this.BK.start("levels");
     }
 
     runGame(episode, level) {
